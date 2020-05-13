4967d2c02f168a02c36569bc6a05732881a3d578
Chirag Rathi <55191179+chiragrathi24@users.noreply.github.com>
2020-05-13 19:31:45 +0530

    Recursion

---
+++ Recursion/Recursion2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fun(int n){
+	static int x=0;
+	if(n>0){
+		x++;
+		return fun(n-1)+x;
+		return 0;
+	}
+}
+int main(){
+	int a=6;
+	cout<<fun(a)<<endl;
+	return 0;
+}
