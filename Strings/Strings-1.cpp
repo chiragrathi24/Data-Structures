aae2725aebf0f2a12314aeaa3b0bdf55c06be9ab
Chirag Rathi <55191179+chiragrathi24@users.noreply.github.com>
2020-05-15 19:37:07 +0530

    Strings

---
+++ Strings/Strings-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+	char a[100];
+	cout<<"Enter the string that is to be printed:"<<endl;
+	gets(a);
+	cout<<"The entered string is:"<<a<<endl;
+	return 0;
+}
