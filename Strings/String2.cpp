fec473cf6ee9741d62f826de825e30b0c7d58874
Chirag Rathi <55191179+chiragrathi24@users.noreply.github.com>
2020-05-22 13:24:11 +0530

    Entering a string

---
+++ Strings/String2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+	char str[100];
+	cout<<"Enter the string:"<<endl;
+	cin.getline(str,80);
+	cout<<"The entered string is:"<<str<<endl;
+	return 0;
+}
